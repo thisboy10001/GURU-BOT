c7b6859f61e5df046a959ca3d41d8f3dd21fa66a
thisboy10001 <163626700+thisboy10001@users.noreply.github.com>
2024-05-17 08:34:52 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['+12067034755', 'My fucking Name Is Mr T-M 🥴💀', true],
+  ['2067034755', 'yeah, its your fucking crazy Boy', true], 
   [''] 
 ] //Number of owners
 
